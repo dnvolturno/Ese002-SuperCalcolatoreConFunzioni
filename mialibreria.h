43102bb8673ded47f9512898f156c52e9aa91b8c
Amedeo Di Gaetano <amegro.agaty@gmail.com>
2015-01-30 09:53:40 +0100

    commit

---
+++ mialibreria.h
@@ -0,0 +1,17 @@
+/* PROTOTIPI DELLE FUNZIONI */
+/* La dichiarazione e la definizione di una funzione possono essere separate.
+ * 
+ * Il prototipo della funzione costituisce l'informazione necessaria al suo
+ * utilizzo: nome, numero e tipo dei parametri di ingresso, tipo del (unico)
+ * parametro di uscita.
+ * 
+ * La definizione della funzione è ottenuta mediante la definizione del suo
+ * corpo. La parte definitoria si trova dopo la funzione main. 
+ */
+float somma(float a, float b);
+float sottrazione(float a, float b);
+float prodotto(float a, float b);
+float divisione(float a, float b);
+int quoziente(int a, int b);
+float frazionale(int a, int b);
+float potenza(float base, int esponente);
